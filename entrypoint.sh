f8c87308c2ed3dea3283cc351822c654b4dc228d
Louie Celli <v-lcelli@youngliving.com>
2024-08-29 17:25:50 -0600

    Adding in the files as a starting point.

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+
+echo "/usr/local/bin/kubectl" >> $GITHUB_PATH
+
+if [ ! -d "$HOME/.kube" ]; then
+    mkdir -p $HOME/.kube
+fi
+
+# Ensure that KUBECONFIG is set
+if [ -z "$KUBECONFIG" ]; then
+  echo "Error: KUBECONFIG environment variable is not set."
+  exit 1
+fi
+
+# If no arguments are provided, print the help and exit
+if [ $# -eq 0 ]; then
+  echo "No command provided. Usage: entrypoint.sh <kubectl-command>"
+  exit 1
+fi
+
+# Execute the kubectl command provided by the user
+echo "Running kubectl command: kubectl $*"
+kubectl "$@"
+
